8bdd3fb6166ba1a6b289084e908fd2218a5caee4
Crynowecer0 <crynowecer11@gmail.com>
2023-03-20 18:20:58 -0400

    all tests pass for omit

--- problems/omit/omit.js
+++ problems/omit/omit.js
@@ -1,4 +1,26 @@
+//given an object and an array, create a new object containing all of the keys
+//from the original object that are NOT present in the array
 
-function omit() { 
-  // Add any parameters you need. Good luck!
+//i - object as obj, array as keys
+//o - a new object
+
+//c - none indicated
+
+//e - aside from missing/invalid input, cannot think of any
+
+function omit(obj, keys) {
+  //create variable to hold a new object, results
+  const results = {};
+
+  //loop over the keys in the argument object
+  for (const key in obj) {
+    //check if the key exists in the argument array
+    //if it does NOT, add the key:value pair to the results object
+    if (!keys.includes(key)) {
+      results[key] = obj[key];
+    }
+  }
+
+  //return the results object declared above
+  return results;
 }
