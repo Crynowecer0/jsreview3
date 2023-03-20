375da13537410ee1e459104305c0b03b197cb6a6
Crynowecer0 <crynowecer11@gmail.com>
2023-03-20 18:46:15 -0400

    tests pass for create matrix and fill

--- problems/createMatrix/createMatrix.js
+++ problems/createMatrix/createMatrix.js
@@ -1,4 +1,12 @@
+function createMatrix(numCols, numRows) {
+  const matrix = [];
 
-function createMatrix() { 
-  // Add any parameters you need. Good luck!
+  for (let row = 0; row < numRows; row++) {
+    let row = [];
+    for (let col = 0; col < numCols; col++) {
+      row.push(0);
+    }
+    matrix.push(row)
+  }
+  return matrix;
 }
--- problems/fill/fill.js
+++ problems/fill/fill.js
@@ -1,4 +1,19 @@
+/* given an array and a value, return the same array with each existing value
+replaced with the argument value */
 
-function fill() { 
-  // Add any parameters you need. Good luck!
+//i - an array and a value
+//o - a modified array
+//c - cannot think of any
+//e - cannot think of any
+
+
+function fill(items, fillVal) {
+  //loop each item in items
+  for (let i = 0; i < items.length; i++) {
+    items[i] = fillVal
+  }
+
+  //replace the existing item with fillval
+  //return items
+  return items
 }
