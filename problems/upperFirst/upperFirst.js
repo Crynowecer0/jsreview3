cbbdea6322b17e4c2727ef882bcaae8b3f4400e3
Crynowecer0 <crynowecer11@gmail.com>
2023-03-21 09:03:40 -0400

    all tests pass for upperfirst

--- problems/upperFirst/upperFirst.js
+++ problems/upperFirst/upperFirst.js
@@ -1,4 +1,14 @@
+/* given a string as input, return a copy of that string with the first character capitalized
+if input is an empty string, return an empty string
+*/
 
-function upperFirst() { 
-  // Add any parameters you need. Good luck!
+//i - a string
+//o - a (new?) string with the first letter capitalized
+//c - empty string needs to return an empty string
+//e - aside from missing/invalid - cannot think of any
+
+function upperFirst(string) {
+  if (string.length === 0) return "";
+
+  return string[0].toUpperCase() + string.slice(1);
 }
