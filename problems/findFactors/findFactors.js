35b1a5e9ad1586e6ee9ac5332151f9defcca9cfe
Crynowecer0 <crynowecer11@gmail.com>
2023-03-21 10:13:07 -0400

    all tests pass for find factors

--- problems/findFactors/findFactors.js
+++ problems/findFactors/findFactors.js
@@ -1,4 +1,12 @@
+/**
+ * given a number, num, return an array that contains every number num is evenly
+ * divisible by, starting with one and going up to and through num itself
+ */
 
-function findFactors() { 
-  // Add any parameters you need. Good luck!
+function findFactors(num) {
+  const results = []
+  for (let i = 0; i <= num; i++) {
+    if (num % i === 0) results.push(i)
+  }
+  return results
 }
