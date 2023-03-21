e9eb926b2b887cceacb3c80318b2a3e3a733b3f1
Crynowecer0 <crynowecer11@gmail.com>
2023-03-21 10:26:44 -0400

    all tests passing for greatest common divisor

--- problems/greatestCommonDivisor/greatestCommonDivisor.js
+++ problems/greatestCommonDivisor/greatestCommonDivisor.js
@@ -1,4 +1,32 @@
+/*
+ *given two whole numbers, return the LARGEST number that evenly divides both numbers
+ * i - two WHOLE numbers
+ * o - the largest number that evenly divdes both numbers
+ * c - both numbers will be whole
+ * e - aside from missing/invalid, none
+ *
+ * psuedocode
+ * set guards against edge cases, if any
+ * declare variable to hold results
+ *
+ * loop from 1 to the smaller of the two numbers
+ * if both numbers divide evenly by the current number
+ * check if the current number is greater than the current value of results
+ * if so, set results equal to the current numbers
+ *
+ * return results
+ */
 
-function greatestCommonDivisor() { 
-  // Add any parameters you need. Good luck!
+function greatestCommonDivisor(num1, num2) {
+  //no edge cases
+  let min = Math.min(...arguments);
+  let results = 0;
+
+  for (let i = 1; i <= min; i++) {
+    if (num1 % i === 0 && num2 % i === 0) {
+      if (i > results) results = i;
+    }
+  }
+
+  return results;
 }
