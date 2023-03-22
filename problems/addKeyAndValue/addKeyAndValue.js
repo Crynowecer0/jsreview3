c22e474f78569e43552e481cc024a4bf03306b00
Crynowecer0 <crynowecer11@gmail.com>
2023-03-22 06:46:40 -0400

    all tests pass for addkeyandvalue

--- problems/addKeyAndValue/addKeyAndValue.js
+++ problems/addKeyAndValue/addKeyAndValue.js
@@ -1,4 +1,21 @@
+/**
+ *
+ *
+ * given an array of objects, a key, and a value, return the same array with
+ * the key and value added to each object
+ *
+ * breakdown
+ * loop over the objects in the objs array
+ * for each object, set a new key value pair equal to the passed in key and value
+ * return the original objs array
+ *
+ */
 
-function addKeyAndValue() { 
-  // Add any parameters you need. Good luck!
+function addKeyAndValue(objs, key, value) {
+
+  for (const object of objs) {
+    object[key] = value;
+  }
+
+  return objs
 }
