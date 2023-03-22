ecdf8c905e2d406a4bd9d3f39b470389d4a8fdf2
Crynowecer0 <crynowecer11@gmail.com>
2023-03-22 14:28:12 -0400

    all tests pass for hasAZero using .some()

--- problems/hasAZero/hasAZero.js
+++ problems/hasAZero/hasAZero.js
@@ -1,4 +1,26 @@
 
-function hasAZero() { 
-  // Add any parameters you need. Good luck!
+/**
+ * given a number, return a boolean indicating if that number contains at least
+ * one zero
+ * use .some or .every to solve this
+ *
+ * create a function to test if a character is equal to 0
+ *
+ * convert the number to an array
+ *
+ * return the result of calling .every on the array, passing in the zero testing
+ * function as the callback
+ *
+ *
+ *
+ *
+ *
+ */
+
+function hasAZero(num) {
+  const inputAsArray = num.toString().split('')
+
+  const isZero = char => char == 0;
+
+  return inputAsArray.some(isZero)
 }
