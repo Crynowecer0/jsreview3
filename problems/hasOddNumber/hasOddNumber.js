c4550eb75f1f9a8b72dd62064bd1d61096f2628a
Crynowecer0 <crynowecer11@gmail.com>
2023-03-22 14:17:19 -0400

    all tests pass for hasOddNumber using .some

--- problems/hasOddNumber/hasOddNumber.js
+++ problems/hasOddNumber/hasOddNumber.js
@@ -1,4 +1,21 @@
+/**
+ * given an array, return a boolean indicating if the array contains at least
+ * one odd number
+ * use .some or .every
+ *
+ * breakdown
+ * loop over each value in the array
+ * test if the current value is an odd number
+ * if it is, return true
+ *
+ * return false
+ *
+ */
 
-function hasOddNumber() { 
-  // Add any parameters you need. Good luck!
+function hasOddNumber(nums) {
+const isOdd = (element) => element % 2 != 0;
+
+return nums.some(isOdd)
 }
+
+
