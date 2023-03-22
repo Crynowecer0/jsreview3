8c53d082e15d7e70681825b31f8004990bab52e0
Crynowecer0 <crynowecer11@gmail.com>
2023-03-22 18:55:16 -0400

    all tests pass for hascertain value using both .some() and .every()

--- problems/hasCertainKey/hasCertainKey.js
+++ problems/hasCertainKey/hasCertainKey.js
@@ -1,4 +1,16 @@
-
-function hasCertainKey() { 
-  // Add any parameters you need. Good luck!
+/**
+ * Given an array of objets and a target key, return a boolean indicating if every
+ * object in the array of object contains the target key
+ * return TRUE if every object contains key
+ * return FALSE if any object does NOT contain key
+ *
+ * breakdown
+ * inputs are array of objects -> objs, target key->targetKey
+ *
+ * use some/every to
+ */
+function hasCertainKey(objs, targetKey) {
+  return objs.every((object, index) => {
+    return (targetKey in object);
+  });
 }
--- problems/hasCertainValue/hasCertainValue.js
+++ problems/hasCertainValue/hasCertainValue.js
@@ -1,4 +1,42 @@
+/**
+ * given an array of objects, a key name, and a target value, return TRUE if every
+ * object in the objects array contains the target value at the input key, otherwise
+ * return false
+ *
+ * inputs:
+ * - array of objects->objs
+ * - a string -> keyName
+ * - any value -> searchValue
+ *
+ * breakdown:
+ * using .some() -> some returns true if any value passes the test
+ *
+ * for the purposes of this problem, needs to return FALSE if any value passes the test
+ *
+ * test -> object[key] === targetVal
+ *
+ * using .every() -> every returns true if ALL values pass the test
+ *
+ * for the purpose of this problem, needs to return true if all values pass the test
+ *
+ * test -> object
+ *
+ *
+ *
+ */
 
-function hasCertainValue() { 
-  // Add any parameters you need. Good luck!
+//implementation with .some()
+// function hasCertainValue(objs, keyName, targetVal) {
+//   //return false if any object has a different value at the target key
+//   return !objs.some((object) => {
+//     return object[keyName] !== targetVal;
+//   });
+// }
+
+//implementation with .every()
+function hasCertainValue(objs, keyName, targetVal) {
+  //return true if all objects have target val at key name
+  return objs.every((object) => {
+    return object[keyName] === targetVal;
+  });
 }
