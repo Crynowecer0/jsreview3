5d92af2585e9339a1069ef3cb73ebddde16b0829
Crynowecer0 <crynowecer11@gmail.com>
2023-03-22 08:04:34 -0400

    tests pass for extract key

--- problems/extractKey/extractKey.js
+++ problems/extractKey/extractKey.js
@@ -1,4 +1,13 @@
 
-function extractKey() { 
-  // Add any parameters you need. Good luck!
+/**
+ * given an array of objects and a key, return a new array containing the value
+ * pointed to by the key argument in each of the objects in the argument array
+ *
+ * use the built in map function
+ */
+function extractKey(objs, keyName) {
+  return objs.map((object, index)=>{
+return object[keyName]
+  })
+
 }
--- problems/valTimesIndex/valTimesIndex.js
+++ problems/valTimesIndex/valTimesIndex.js
@@ -1,9 +1,7 @@
-
 /**
  * given an array of numbers, return a new array containing the result of of multiplying
  * each number in the input array by the index at which it appears
  */
 function valTimesIndex(array) {
-
-  return array.map((num, index)=>num*index)
+  return array.map((num, index) => num * index);
 }
