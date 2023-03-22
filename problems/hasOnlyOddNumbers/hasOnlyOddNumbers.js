45f3d3f66007a2340ec6db05b3c01fc200f61a3a
Crynowecer0 <crynowecer11@gmail.com>
2023-03-22 14:46:15 -0400

    tests pass for hasonlyodd, both some and every

--- problems/hasOnlyOddNumbers/hasOnlyOddNumbers.js
+++ problems/hasOnlyOddNumbers/hasOnlyOddNumbers.js
@@ -1,4 +1,21 @@
+/**
+ * given an array of numbers, return a boolean indicting if ANY single value is odd
+ * use .some or .every
+ *
+ * every returns true if ALL values pass a test
+ * some returns true if ANY value passes a test
+ */
 
-function hasOnlyOddNumbers() { 
-  // Add any parameters you need. Good luck!
+function hasOnlyOddNumbers(nums) {
+
+
+  //implementation of .some()
+  const isEven = (num) => num % 2 == 0;
+  return !nums.some(isEven);
+
+  // implementation of .every()
+  // const isOdd = (num) => num % 2 != 0;
+  // return nums.every(isOdd)
 }
+
+hasOnlyOddNumbers([1, 3, 5]);
