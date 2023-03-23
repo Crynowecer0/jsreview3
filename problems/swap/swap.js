747b653762977fd0dd6a4930cc2e53f2109812a0
Crynowecer0 <crynowecer11@gmail.com>
2023-03-22 21:10:52 -0400

    tests pass for swap

--- problems/swap/swap.js
+++ problems/swap/swap.js
@@ -1,4 +1,11 @@
-
-function swap() { 
-  // Add any parameters you need. Good luck!
+/**
+ * given an array and two indexes, return a mutated array where the values at the
+ * indexes passed into the function are swapped
+ *
+ * simple to solve with destructuring swap syntax
+ *
+ */
+function swap(items, index1, index2) {
+  [items[index1], items[index2]] = [items[index2], items[index1]]
+  return items;
 }
